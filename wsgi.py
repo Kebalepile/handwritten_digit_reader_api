dba994290d408585de9f33bd6ffa4da2184812e3
Kebalepile <pimpabutterfly86@gmail.com>
2024-06-22 15:23:48 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
-from apis.predict_api_v_0 import  load_model_before_fork, app
+from apis.predict_api_v_0 import   app
 
 if __name__ == '__main__':
-    load_model_before_fork()
     app.run(host='0.0.0.0', port=10000)
