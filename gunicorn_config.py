9509db58efc77e02b7e2ff33d6956f184dac5ee2
Kebalepile <pimpabutterfly86@gmail.com>
2024-06-22 09:32:15 +0200

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,6 @@
 # gunicorn_config.py
 bind = "0.0.0.0:8000"
 workers = 4  # Adjust the number of workers based on your server's capacity
-timeout = 180  # Set the timeout to 180 seconds (3 minutes)
+# timeout = 180  # Set the timeout to 180 seconds (3 minutes)
+timeout = 600 # Set the timeout to 600 seconds (10 minutes)
+
