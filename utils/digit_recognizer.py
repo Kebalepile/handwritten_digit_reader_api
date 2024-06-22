62ca9c3081c49d2f6d593baa0f0f8008bce9909e
Kebalepile <pimpabutterfly86@gmail.com>
2024-06-22 12:37:09 +0200

    Update digit_recognizer.py

--- utils/digit_recognizer.py
+++ utils/digit_recognizer.py
@@ -1,21 +1,26 @@
 import os
 import numpy as np
+import logging
 from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def init_model(model_name):
     try:
-        path = os.path.join(os.path.dirname(__file__), '..','models', model_name)  # Adjust path as necessary
+        path = os.path.join(os.path.dirname(__file__), '..', 'models', model_name)  # Adjust path as necessary
+        logger.info(f"Loading model from {path}")
         model = load_model(path)
+        logger.info("Model loaded successfully.")
         return model
     except Exception as e:
-        print(f"Error loading the model: {e}")
+        logger.error(f"Error loading the model: {e}")
         exit(1)
 
 def prepare_image(image, target_size):
-   
     if image.mode != 'L':
         image = image.convert('L')
     image = image.resize(target_size)
